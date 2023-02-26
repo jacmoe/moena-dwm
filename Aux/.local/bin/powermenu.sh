916e50350b9933e8bad9f51b8c7edb9567e6c1aa
jacmoe <jacmoe.dk@gmail.com>
2023-02-26 17:14:08 +0100

    Much better powermenu script: a one-liner

--- Aux/.local/bin/powermenu.sh
+++ Aux/.local/bin/powermenu.sh
@@ -1,13 +1,2 @@
 #!/usr/bin/bash
-
-function powermenu {
-	options="Cancel\nShutdown\nRestart\n"
-	selected=$(echo -e $options | dmenu)
-	if [[ $selected = "Shutdown" ]]; then
-		systemctl poweroff
-	elif [[ $selected = "Restart" ]]; then
-		systemctl reboot
-	fi
-}
-
-powermenu
+echo -e "poweroff\nreboot\nsuspend" | dmenu | xargs systemctl
