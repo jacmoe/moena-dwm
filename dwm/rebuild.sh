8b7e7ca539a7c191d18675c19496bca035a9ee47
jacmoe <jacmoe.dk@gmail.com>
2023-02-26 17:42:37 +0100

    Rebuild script; Dark Magenta theme fixes

---
+++ dwm/rebuild.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rm -f config.h && sudo make clean install
--- dwm/themes/dark-magenta.h
+++ dwm/themes/dark-magenta.h
@@ -6,7 +6,7 @@ static const char col_4[]  = "#E06C75";
 static char normfgcolor[]                = "#bbbbbb";
 static char normbgcolor[]                = "#222222";
 static char normbordercolor[]            = "#444444";
-static char normfloatcolor[]             = "#db8fd9";
+static char normfloatcolor[]             = "#c678dd";
 
 static char selfgcolor[]                 = "#eeeeee";
 static char selbgcolor[]                 = "#c678dd";
@@ -16,7 +16,7 @@ static char selfloatcolor[]              = "#c678dd";
 static char titlenormfgcolor[]           = "#bbbbbb";
 static char titlenormbgcolor[]           = "#222222";
 static char titlenormbordercolor[]       = "#444444";
-static char titlenormfloatcolor[]        = "#db8fd9";
+static char titlenormfloatcolor[]        = "#c678dd";
 
 static char titleselfgcolor[]            = "#eeeeee";
 static char titleselbgcolor[]            = "#ac3ba8";
@@ -26,7 +26,7 @@ static char titleselfloatcolor[]         = "#ac3ba8";
 static char tagsnormfgcolor[]            = "#bbbbbb";
 static char tagsnormbgcolor[]            = "#222222";
 static char tagsnormbordercolor[]        = "#444444";
-static char tagsnormfloatcolor[]         = "#db8fd9";
+static char tagsnormfloatcolor[]         = "#c678dd";
 
 static char tagsselfgcolor[]             = "#eeeeee";
 static char tagsselbgcolor[]             = "#ac3ba8";
@@ -34,7 +34,7 @@ static char tagsselbordercolor[]         = "#ac3ba8";
 static char tagsselfloatcolor[]          = "#ac3ba8";
 
 static char hidnormfgcolor[]             = "#c678dd";
-static char hidselfgcolor[]              = "#db8fd9";
+static char hidselfgcolor[]              = "#c678dd";
 static char hidnormbgcolor[]             = "#222222";
 static char hidselbgcolor[]              = "#222222";
 
