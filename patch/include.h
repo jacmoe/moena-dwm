01d0624f752cfdd8181eef00cd9d0148e2f2a78b
jacmoe <jacmoe.dk@gmail.com>
2023-02-26 12:02:09 +0100

    Initial

---
+++ patch/include.h
@@ -0,0 +1,26 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "autostart.h"
+#include "cyclelayouts.h"
+#include "movestack.h"
+#include "rotatestack.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_bstack.h"
+#include "layout_bstackhoriz.h"
+#include "layout_grid.h"
+#include "layout_horizgrid.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
---
+++ themes/onedark.h
@@ -0,0 +1,44 @@
+	/*                       fg                bg                border                float */
+
+static const char col_3[]  = "#c678DD";
+static const char col_4[]  = "#E06C75";
+
+static char normfgcolor[]                = "#bbbbbb";
+static char normbgcolor[]                = "#222222";
+static char normbordercolor[]            = "#444444";
+static char normfloatcolor[]             = "#db8fd9";
+
+static char selfgcolor[]                 = "#eeeeee";
+static char selbgcolor[]                 = "#c678dd";
+static char selbordercolor[]             = "#c678dd";
+static char selfloatcolor[]              = "#c678dd";
+
+static char titlenormfgcolor[]           = "#bbbbbb";
+static char titlenormbgcolor[]           = "#222222";
+static char titlenormbordercolor[]       = "#444444";
+static char titlenormfloatcolor[]        = "#db8fd9";
+
+static char titleselfgcolor[]            = "#eeeeee";
+static char titleselbgcolor[]            = "#ac3ba8";
+static char titleselbordercolor[]        = "#ac3ba8";
+static char titleselfloatcolor[]         = "#ac3ba8";
+
+static char tagsnormfgcolor[]            = "#bbbbbb";
+static char tagsnormbgcolor[]            = "#222222";
+static char tagsnormbordercolor[]        = "#444444";
+static char tagsnormfloatcolor[]         = "#db8fd9";
+
+static char tagsselfgcolor[]             = "#eeeeee";
+static char tagsselbgcolor[]             = "#ac3ba8";
+static char tagsselbordercolor[]         = "#ac3ba8";
+static char tagsselfloatcolor[]          = "#ac3ba8";
+
+static char hidnormfgcolor[]             = "#c678dd";
+static char hidselfgcolor[]              = "#db8fd9";
+static char hidnormbgcolor[]             = "#222222";
+static char hidselbgcolor[]              = "#222222";
+
+static char urgfgcolor[]                 = "#bbbbbb";
+static char urgbgcolor[]                 = "#222222";
+static char urgbordercolor[]             = "#ff0000";
+static char urgfloatcolor[]              = "#E06C75";
