03e19fafc2cf98e9cf19e4f48c67cd866e5e8511
jacmoe <jacmoe.dk@gmail.com>
2023-04-04 12:09:57 +0200

    Removing quit command

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -215,7 +215,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return,     zoom,                   {0} },
 	{ MODKEY,                       XK_Tab,        view,                   {0} },
 	{ MODKEY|ShiftMask,             XK_c,          killclient,             {0} },
-	{ MODKEY|ShiftMask,             XK_q,          quit,                   {0} },
+	/*{ MODKEY|ShiftMask,             XK_q,          quit,                   {0} },*/
 	{ MODKEY,                       XK_t,          setlayout,              {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,          setlayout,              {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,          setlayout,              {.v = &layouts[2]} },
