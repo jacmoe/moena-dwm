1b6a736fc743e9cf416348ee0f72478b872f2cd0
jacmoe <jacmoe.dk@gmail.com>
2023-04-04 11:52:05 +0200

    setxkbmap does not work ...

--- Aux/.local/share/dwm/autostart.sh
+++ Aux/.local/share/dwm/autostart.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 ibus-daemon -drxR &
 /usr/libexec/polkit-gnome-authentication-agent-1 &
 numlockx on
 nitrogen --restore &
-setxkbmap -layout us,no -variant colemak,colemak -option 'grp:shifts_toggle'
 slstatus &
+setxkbmap -layout us,no -variant colemak,colemak -option 'grp:shifts_toggle'
