31e02fa483be06859c2310a1e7dd0c461ff2841d
jacmoe <jacmoe.dk@gmail.com>
2023-07-15 20:14:13 +0200

    Now using xfce-terminal

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -183,7 +183,7 @@ static const char *dmenucmd[] = {
 	NULL
 };
 static const char *roficmd[] = { "rofi", "-show", "drun", "-icon-theme", "'papirus'", "-show-icons", NULL};
-static const char *termcmd[]  = { "wezterm", NULL };
+static const char *termcmd[]  = { "xfce-terminal", NULL };
 static const char *appscmd[]  = { "/home/moena/.local/bin/apps.sh", NULL };
 static const char *utilscmd[]  = { "/home/moena/.local/bin/utils.sh", NULL };
 static const char *powercmd[]  = { "/home/moena/.local/bin/powermenu", NULL };
