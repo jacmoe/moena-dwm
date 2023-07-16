318706470b1a6f7ac5eeef11c29569f6cac289c0
jacmoe <jacmoe.dk@gmail.com>
2023-07-16 17:53:06 +0200

    Autostart - picom

--- Aux/.local/share/dwm/autostart.sh
+++ Aux/.local/share/dwm/autostart.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-ibus-daemon -drxR &
-/usr/libexec/polkit-gnome-authentication-agent-1 &
-numlockx on
+#dbus-run-session dwm &
+#ibus-daemon -drxR &
+#dbus-launch /usr/libexec/polkit-gnome-authentication-agent-1 &
+#dbus-launch /usr/lib/xfce-polkit/xfce-polkit &
+#numlockx on
+picom &
 nitrogen --restore &
 slstatus &
 setxkbmap -layout us,no -variant colemak,colemak -option 'grp:shifts_toggle'
